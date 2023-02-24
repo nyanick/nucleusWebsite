b550f3feca64d6565edd9fa79cc94476419008ff
nyanick <yanicknde@gmail.com>
2023-02-24 15:53:51 +0100

    Finsihed diagnostic and anoucements

--- src/pages/advertisements/index.tsx
+++ src/pages/advertisements/index.tsx
@@ -1,55 +1,121 @@
 import {NextPage} from "next";
-// import React from "react";
-// import {useFindNurses} from "../../hooks/api/nurse";
-// import DataTable from "react-data-table-component";
+import React, { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import {useStateValue} from "../../contexts/AuthProvider";
+import {useFindUser} from "../../hooks/api/user";
+import {useFindDiagnosisByHospitalId} from "../../hooks/api/diagnosis";
+import axios from "axios";
+import moment from "moment";
+import { apiRoutes } from "../../constants";
+import { IConsultation } from "../../types/consultation";
+import { IDiagnosis } from "../../types/diagnosis";
+import {FiSettings, FiUsers} from "react-icons/fi";
+import Link from "next/link";
 
-// const Advertisement: NextPage = ({}) => {
-//     const {data: advertisements} = useFindNurses();
-//     let advertisementNumber = 0;
-//     if (advertisements) {
-//         advertisementNumber = advertisements.length
-//     }
+const DiagnosticsList: NextPage = ({}) => {
+    const [{authUser}] = useStateValue();
+    const [{adminHospital}] = useStateValue();
 
-//     const columns = [
-//         {
-//             name: 'Noms',
-//             selector: row => row.firstName + ' ' + row.lastName,
-//             sortable: true,
-//         },
-//         {
-//             name: 'Téléphone',
-//             selector: 'phoneNumber',
-//             sortable: true,
-//         },
-//         {
-//             name: 'Email',
-//             selector: 'email',
-//             sortable: true,
-//         },
-//         {
-//             name: 'DOB',
-//             selector: 'dob',
-//             sortable: false,
-//         },
-//     ];
 
-//     return (
-//         <section className='p-8'>
-//             <div className='space-y-8'>
-//                 <div className='w-full border-x border-t rounded-lg'>
-//                     <DataTable
-//                         title={advertisementNumber + " Annonces"}
-//                         columns={columns}
-//                         data={advertisements}
-//                         pagination
-//                         responsive={true}
+    const [formData, setFormData] = useState({
+        description: ""
+    });
 
-//                         highlightOnHover
-//                     />
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// };
+    useEffect(async ()=> {
+        let description = null;
 
-// export default Advertisement;
+    },[]);
+
+
+    const handleInput = (e) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+
+        setFormData((prevState) => ({
+        ...prevState,
+        [fieldName]: fieldValue
+        }));
+    }
+
+    const addAnnoucement = async (event) => {
+        event.preventDefault()
+
+        const formURL = event.target.action
+        const data = new FormData()
+
+        Object.entries(formData).forEach(([key, value]) => {
+            data.append(key, value);
+        })
+
+    };
+
+    const columns = [
+        {
+            name: 'Noms',
+            selector: row => row.firstName + ' ' + row.lastName,
+            sortable: true,
+        },
+        {
+            name: 'Email',
+            selector: row => row.email,
+            sortable: true,
+        },
+        {
+            name: 'Phone',
+            selector: row => row.phoneNumber,
+            sortable: true,
+        },
+        {
+            name: 'Consultation',
+            selector: row => row.specialtyId,
+            sortable: true,
+        },
+    ];
+
+    return (
+        <>
+            
+
+            <section className='p-8'>
+                <div className='space-y-8'>
+
+                    <div>
+                        <div>
+                            <form onSubmit={addAnnoucement}>
+                                <div className="grid grid-cols-1 gap-6">
+                                    <div className="mt-0">
+                                    <label className="block text-gray-700 text-sm font-bold ">Annoucement(s) </label>
+                                        <textarea id="about" name="about" rows="5" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" ></textarea>
+                                    </div>
+
+                                    <div className="md:flex md:items-center ">
+                                        <div >
+                                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-4 rounded focus:outline-none focus:shadow-outline" type="button" type="submit">Add</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className='w-full border-x border-t rounded-lg'>
+                        <DataTable
+                            title={[].length + " Annoucement"}
+                            columns={columns}
+                            data={[]}
+                            pagination
+                            responsive={true}
+                            highlightOnHover
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
+        
+    )
+};
+
+
+
+export default DiagnosticsList;
---
+++ src/pages/diagnostics/index.tsx
@@ -0,0 +1,183 @@
+import {NextPage} from "next";
+import React, { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import {useStateValue} from "../../contexts/AuthProvider";
+import {useFindUser} from "../../hooks/api/user";
+import {useFindDiagnosisByHospitalId} from "../../hooks/api/diagnosis";
+import axios from "axios";
+import moment from "moment";
+import { apiRoutes } from "../../constants";
+import { IConsultation } from "../../types/consultation";
+import { IDiagnosis } from "../../types/diagnosis";
+import {FiSettings, FiUsers} from "react-icons/fi";
+import Link from "next/link";
+
+const DiagnosticsList: NextPage = ({}) => {
+    const [{authUser}] = useStateValue();
+    const [{adminHospital}] = useStateValue();
+    const [diagnosis, setDiagnosis] = useState([]);
+
+
+    const [formData, setFormData] = useState({
+        start_date: "",
+        end_date: "",
+        description: ""
+    });
+
+    useEffect(async ()=> {
+        let start_date = null;
+        let end_date = null;
+        let description = null;
+
+        let diagnosis = await findDiagnostics(start_date, end_date, adminHospital, description);
+        console.log('diagnosis',diagnosis)
+        setDiagnosis(diagnosis);
+
+    },[ adminHospital, setDiagnosis]);
+
+
+    const handleInput = (e) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+
+        setFormData((prevState) => ({
+        ...prevState,
+        [fieldName]: fieldValue
+        }));
+    }
+
+    const searchDiagnostic = async (event) => {
+        event.preventDefault()
+
+        const formURL = event.target.action
+        const data = new FormData()
+
+        Object.entries(formData).forEach(([key, value]) => {
+            data.append(key, value);
+        })
+
+        let start_date = moment(formData.start_date, "yyyy-MM-ddTHH:mm").toISOString();
+        let end_date =  moment(formData.end_date, "yyyy-MM-ddTHH:mm").toISOString();
+        let description = formData.description;
+
+        let diagnosis = await findDiagnostics(start_date, end_date, adminHospital, description);
+        setDiagnosis(diagnosis);
+
+    };
+
+    const columns = [
+        {
+            name: 'Description',
+            selector: row => row.firstName + ' ' + row.lastName,
+            sortable: true,
+        },
+        {
+            name: 'Updated Date',
+            selector: row => moment(row.updatedAt).toDate().toLocaleString(),
+            sortable: true,
+        },
+        {
+            name: "Consultations",
+            button: true,
+            cell: (row) => (
+                <button
+                    className="btn btn-outline btn-xs"
+                    onClick={(e) => handleButtonClick(e, row.consultationId)}
+                >
+                    <FiEye/>
+                </button>
+            ),
+        },
+    ];
+
+    return (
+        <>
+            <div>
+                <div>
+                    <form onSubmit={searchDiagnostic}>
+                        <div className="flex items-center justify-center space-x-4 mt-2 ">
+
+                            <div className="md:flex md:items-center ">
+                                <div className="">
+                                <label className="block text-gray-700 text-sm font-bold ">Description:</label>
+
+                                </div>
+                            </div>
+
+                            <div className="md:flex md:items-center ">
+                                <div className="">
+                                    <input  className="form-control block w-full text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"  type="text" id="start_date" name="description" onChange={handleInput} value={formData.description}  />
+
+                                </div>
+                            </div>
+
+                            <div className="md:flex md:items-center ">
+                                <div className="">
+                                <label className="block text-gray-700 text-sm font-bold ">Start Date:</label>
+
+                                </div>
+                            </div>
+
+                            <div className="md:flex md:items-center ">
+                                <div className="">
+                                    <input  className="form-control block w-full text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"  type="datetime-local" id="start_date" name="start_date" onChange={handleInput} value={formData.start_date} required />
+
+                                </div>
+                            </div>
+
+                            <div className="md:flex md:items-center ">
+                                <div >
+                                    <label className="block text-gray-700 text-sm font-bold " >End Date:</label>
+                                </div>
+                            </div>
+
+                            <div className="md:flex md:items-center ">
+                                <div >
+                                    <input  className="form-control block w-full text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="datetime-local" id="end_date" name="end_date" onChange={handleInput} value={formData.end_date} required />
+                                </div>
+                            </div>
+
+                            <div className="md:flex md:items-center ">
+                                <div >
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-4 rounded focus:outline-none focus:shadow-outline" type="button" type="submit">Search</button>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <section className='p-8'>
+                <div className='space-y-8'>
+                    <div className='w-full border-x border-t rounded-lg'>
+                        <DataTable
+                            title={diagnosis.length + " Diagnosis"}
+                            columns={columns}
+                            data={diagnosis}
+                            pagination
+                            responsive={true}
+                            highlightOnHover
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
+        
+    )
+};
+
+const findDiagnostics = async (start_date, end_date,adminHospital, description) =>{
+    if(!start_date || !end_date || !adminHospital){
+        return [];
+    }
+    let diagnosis =  await axios.get<IDiagnosis[]>(`${apiRoutes.CONSULTATIONS}/diagnosis/hospital/${adminHospital}`,{
+        params: {start_date: start_date, end_date: end_date,description:description }
+    });
+    if(diagnosis && diagnosis.data){
+        return diagnosis.data;
+    }
+    return [];
+}
+
+export default DiagnosticsList;
