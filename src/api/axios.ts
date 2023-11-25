4fce401f56a2dc0acc63c955b8d1ce9f0051f5b9
nyanick <yanicknde@gmail.com>
2023-11-25 12:45:51 +0100

    Authorized self trusted certificate

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,6 +7,9 @@ const http = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         "Content-type": "application/json"
+    },
+    httpsAgent: {
+        rejectUnauthorized: false
     }
 });
 
